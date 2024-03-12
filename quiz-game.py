3bdcf750883e4dd8bdbee0de104517efff8764e4
Drenii <dreni107@hotmail.com>
2024-03-12 02:34:08 +0100

    quiz game completed

--- quiz-game.py
+++ quiz-game.py
@@ -6,3 +6,40 @@ if playing.lower() != 'yes':
     quit()
 
 print('Welcome to my quiz game')
+score=0
+
+answer=input('what cpu stands for? ')
+
+if answer.lower() == 'central processing unit':
+    print('You are correct')
+    score=score+1
+else:
+    print('You are wrong')
+
+
+answer=input('what is 1+1 equal to? ')
+
+if answer.lower() == '2':
+    print('You are correct')
+    score=score+1
+else:
+    print('You are wrong')
+
+answer=input('what gpu stands for? ')
+
+if answer.lower() == 'graphical processing unit':
+    print('You are correct')
+    score=score+1
+else:
+    print('You are wrong')
+
+answer=input('which is the third month of the year? ')
+
+if answer.lower() == 'march':
+    print('You are correct')
+    score=score+1
+else:
+    print('You are wrong')
+
+print('you got '+ str(score)+ ' correct answers')
+print('you got '+ str((score/4)*100)+ ' %')
